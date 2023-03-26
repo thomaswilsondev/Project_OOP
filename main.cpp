72deb0244da8b4ccc3e1d398acd2a5aa2c314305
Lê Tấn Bảo Bảo <110554959+thomaswilsondev@users.noreply.github.com>
2023-03-26 23:48:37 +0700

    add classes Supplier, OrderItem and Order

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <fstream>
 #include <sstream>
 #include <iomanip>
-
+#include <ctime>
 using namespace std;
 
 class Person
@@ -28,13 +28,6 @@ protected:
 public:
 };
 
-class Order
-{
-private:
-protected:
-public:
-};
-
 class Employee
 {
 private:
@@ -45,16 +38,187 @@ public:
 class Supplier : public Person
 {
 private:
+    int supplierId;
+    string companyName;
+    string taxCode;
+
 protected:
 public:
+    // Getter
+    int getSupplierId() { return supplierId; };
+    string getCompanyName() { return companyName; };
+    string getTaxCode() { return taxCode; };
+    // Setter
+    void setSupplierId(int supplierId)
+    {
+        this->supplierId = supplierId;
+    };
+    void setCompanyName(string companyName)
+    {
+        this->companyName = companyName;
+    }
+    void setTaxCode(string taxCode)
+    {
+        this->taxCode = taxCode;
+    }
+    // Contructor
+    Supplier(int supplierId, string companyName, string taxCode)
+    {
+        this->supplierId = supplierId;
+        this->companyName = companyName;
+        this->taxCode = taxCode;
+    }
+    ~Supplier(){};
 };
 class OrderItem : public Person
 {
 private:
+    int orderItemId;
+    string product;
+    int quantity;
+    int price;
+
 protected:
 public:
-};
+    // GETTER
+    int getOrderItemId() { return orderItemId; };
+    string getProduct() { return product; };
+    int getQuantity() { return quantity; };
+    double getPrice() { return price; };
+    // SETTER
+    void setOrderItemId(int orderItemId)
+    {
+        this->orderItemId = orderItemId;
+    }
+    void setProduct(string product)
+    {
+        this->product = product;
+    }
+    void setQuantity(int quantity)
+    {
+        this->quantity = quantity;
+    }
+    void setPrice(int price)
+    {
+        this->price = price;
+    }
 
+    // CONTRUCTOR
+    OrderItem(int orderItemId, string product, int quantity, int price)
+    {
+        this->orderItemId = orderItemId;
+        this->product = product;
+        this->quantity = quantity;
+        this->price = price;
+    }
+};
+class Order
+{
+private:
+    int orderId;
+    string customer;
+    vector<OrderItem> orderItem;
+    vector<OrderItem> totalPrice;
+    string orderDate;
+
+protected:
+public:
+    int indexOrderID = 0;
+    // GETTER
+    int getOrderId() { return orderId; };
+    string getCustomer() { return customer; };
+    void getOrderItem(vector<OrderItem> orderItem)
+    {
+        for (int i = 0; i < orderItem.size(); i++)
+        {
+            cout << "Product " << orderItem[i].getOrderItemId() << " :"
+                 << orderItem[i].getProduct() << " ,"
+                 << orderItem[i].getQuantity() << " ,"
+                 << orderItem[i].getPrice();
+        }
+    }
+    int getTotalPrice(vector<OrderItem> orderItem)
+    {
+        int sum = 0;
+        for (int i = 0; i < orderItem.size(); i++)
+        {
+            sum = sum + orderItem[i].getPrice() * orderItem[i].getQuantity();
+        }
+        return sum;
+    }
+    void getOrderDate(string orderDate)
+    {
+        this->orderDate = orderDate;
+    }
+    // SETTER
+    int setTotalPrice(vector<OrderItem> orderItem, int tip)
+    {
+        int total = getTotalPrice(orderItem) + getTotalPrice(orderItem) * (tip / 100);
+        return total;
+    }
+    void setCustomer(string customer)
+    {
+        this->customer = customer;
+    }
+    void setOrderID()
+    {
+        this->orderId = indexOrderID++;
+    }
+    void setOrderItem(vector<OrderItem> orderItem)
+    {
+        for (int i = 0; i < orderItem.size(); i++)
+        {
+            string producer;
+            int quantity = 0;
+            double price = 0;
+            orderItem[i].setOrderItemId(i);
+            cout << "Product " << orderItem[i].getOrderItemId() << endl;
+            cout << "Enter name Producer: ";
+            cin >> producer;
+            orderItem[i].setProduct(producer);
+            cout << "Enter Quantity: ";
+            cin >> quantity;
+            orderItem[i].setQuantity(quantity);
+            cout << "Enter Price: ";
+            cin >> price;
+            orderItem[i].setPrice(price);
+            cout << "\n\n";
+        }
+    }
+    void setOrderDate()
+    {
+        time_t now = time(0);
+        tm *ltm = localtime(&now);
+        cout << "Time: " << ltm->tm_sec << endl;
+        cout << "Date: " << ltm->tm_mday << "/" << 1 + ltm->tm_mon << "/" << 1900 + ltm->tm_year;
+    }
+    void addOrderIteam(vector<OrderItem> orderItem)
+    {
+        string producer;
+        int quantity = 0;
+        double price = 0;
+        orderItem[orderItem.size()].setOrderItemId(orderItem.size());
+        cout << "Product " << orderItem[orderItem.size()].getOrderItemId() << endl;
+        cout << "Enter name Producer: ";
+        cin >> producer;
+        orderItem[orderItem.size()].setProduct(producer);
+        cout << "Enter Quantity: ";
+        cin >> quantity;
+        orderItem[orderItem.size()].setQuantity(quantity);
+        cout << "Enter Price: ";
+        cin >> price;
+        orderItem[orderItem.size()].setPrice(price);
+        cout << "\n\n";
+    }
+    // CONTRUCTOR
+    Order(int orderId, string customer, vector<OrderItem> orderItem, string orderDate)
+    {
+        this->orderId = orderId;
+        this->customer = customer;
+        this->orderItem = orderItem;
+        this->orderDate = orderDate;
+    }
+};
 class Product
 {
 private:
@@ -262,20 +426,47 @@ public:
 };
 int main()
 {
-    string filename = "product.csv";
-    ProductFile data;
-    cout << "Dau vao" << endl;
-    ControllProduct controller(data.loadProduct(filename));
-    controller.Display();
+    // string filename = "product.csv";
+    // ProductFile data;
+    // cout << "Dau vao" << endl;
+    // ControllProduct controller(data.loadProduct(filename));
+    // controller.Display();
 
-    cout << "\n\nSau khi them" << endl;
+    // cout << "\n\nSau khi them" << endl;
 
-    controller.addProduct(Product(7, "Cat", "Cat bien vung tau", "Cat ok xay nha bao dep", 1000, 100));
-    controller.Display();
+    // controller.addProduct(Product(7, "Cat", "Cat bien vung tau", "Cat ok xay nha bao dep", 1000, 100));
+    // controller.Display();
 
-    cout << "\n\nSau khi xoa" << endl;
-    controller.removeProduct(5);
-    controller.Display();
+    // cout << "\n\nSau khi xoa" << endl;
+    // controller.removeProduct(5);
+    // controller.Display();
+
+    //--------------Supplier----------------------
+    // Supplier s(1, "ABC Company", "123456");
+    // cout << "Supplier ID: " << s.getSupplierId() << endl;
+    // cout << "Company Name: " << s.getCompanyName() << endl;
+    // cout << "Tax Code: " << s.getTaxCode() << endl;
+    // s.setCompanyName("XYZ Company");
+    // cout << "New Company Name: " << s.getCompanyName() << endl;
+
+    //---------------OrderItem-------------------------
+    // OrderItem oi(1, "Product A", 2, 10);
+    // cout << "Order Item ID: " << oi.getOrderItemId() << endl;
+    // cout << "Product: " << oi.getProduct() << endl;
+    // cout << "Quantity: " << oi.getQuantity() << endl;
+    // cout << "Price: " << oi.getPrice() << endl;
+    // oi.setQuantity(3);
+    // cout << "New Quantity: " << oi.getQuantity() << endl;
+
+    //---------------Order---------------------------
+    // OrderItem oi1(1, "Product A", 2, 10);
+    // OrderItem oi2(2, "Product B", 3, 20);
+    // vector<OrderItem> items = {oi1, oi2};
+    // Order o(1, "Customer A", items, "2022-01-01");
+    // cout << "Order ID: " << o.getOrderId() << endl;
+    // cout << "Customer: " << o.getCustomer() << endl;
+    // o.getOrderItem(items);
+    // cout << "\nTotal Price: " << o.getTotalPrice(items) << endl;
 
     return 0;
 }
